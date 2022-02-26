aef69332a00f073effb8b543c837d97166bcc71b
Sandro Spengler Macbook <sandro.spengler@web.de>
2022-02-26 08:22:24 +0100

    Basic Routing

---
+++ src/Pages/Overview.tsx
@@ -0,0 +1,17 @@
+// import { Card, CardContent, Grid, Paper, Typography } from "@mui/material";
+
+const Overview = (props: any) => {
+  //   const cardStyles = {
+  //     backgroundColor: "#182333",
+  //     color: "white",
+  //     padding: "20px",
+  //     width: {
+  //       xs: "100%",
+  //       lg: "100%",
+  //     },
+  //   };
+
+  return <p>Hello World</p>;
+};
+
+export default Overview;
